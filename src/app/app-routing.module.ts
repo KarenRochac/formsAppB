a432e7f928c31a941603bb03d45d7866fb4cd796
Miguel Jose Albano da Silva <61022078+josemiguel2004@users.noreply.github.com>
2022-11-03 15:01:31 -0300

    03/11/22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro/registro.module').then( m => m.RegistroPageModule)
   }
 ];
 @NgModule({
